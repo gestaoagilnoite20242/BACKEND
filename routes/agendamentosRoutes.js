5ddeb42042da10674dde80b93cfc78f1bb13dbf2
Joao Luis Rosa de Moura <Joao.Moura@zaffari.com.br>
2024-10-29 14:26:13 -0300

    Modificação nos endpoints de agendamento e acrescimo de endpoint

--- routes/agendamentosRoutes.js
+++ routes/agendamentosRoutes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const agendamentosController = require('../controllers/agendamentosController');
 const router = express.Router();
 
-router.get('/agendamentos/:prestador_id', agendamentosController.getAgendamentosByPrestadorId);
-router.get('/agendamentos/', agendamentosController.getAgendamentos);
+router.get('/agendamentos/:prestador_id', agendamentosController.getAgendByPrestId);
+router.get('/agendamentos/:prestador_id/:data_inicio/:data_fim', agendamentosController.getAgendByPrestIdBetween);
+router.get('/agendamentosFuturos/:prestador_id', agendamentosController.getAgendFuturByPrestId);
 
 
 module.exports = router;
